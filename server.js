d098900c49037393a7e5379df510e812339a948b
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-06-04 23:00:57 +0600

    mongo db connected

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
+
+//connect the database
+
+connectDB();
+
 
 app.get('/',(req,res) => {
     res.send('HELLO WORLD!!!');
