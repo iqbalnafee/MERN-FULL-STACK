60b9ef7cdfbdc0c3ec546bdfc9bde2b14325bc9c
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-06-05 21:07:51 +0600

    user registration backend code added

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ const connectDB = async () => {
     try{
         await mongoose.connect(db,{
             useNewUrlParser: true,
-            useUnifiedTopology: true 
+            useUnifiedTopology: true,
+            useCreateIndex:true 
         });
         console.log('MongoDB connected.....');
     }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs')
+
+//we need to require our model 
+
+const User = require('../../models/User');
+
 const { check,validationResult } = require('express-validator');
 
 //https://express-validator.github.io/docs/ for reference that any parameter has any error
@@ -15,14 +22,61 @@ router.post('/',[
     check('email','Please enter a valid email').isEmail(),
     check('password','Please enter a password with 6 or more character').isLength({min:6})
 
-], (req,res) => {
+], 
+async (req,res) => {
 
     const error = validationResult(req);
     if(!error.isEmpty()){
         return res.status(400).json({ error: error.array()});
     }
 
-    res.send('User route');
+    //we can get form data by using req.body.name but we dont use it
+
+    //we destructure it by following way because
+
+    const {name,email,password} = req.body;
+
+    try{
+
+        //see if user exists
+
+        let user = await User.findOne({ email });
+
+        if(user){
+            return res.status(400).json({ errors: [{msg: 'User already exists'}] });
+        }
+
+        //get users avatar
+
+        const avatar = gravatar.url(email,{
+
+            s:'200',//size of the image
+            r:'pg',//rating eg. no naked/inappropriate image
+            d:'mm'//default eg. if  avatar absent there should be some icon
+
+        });
+
+        user = new User({name,email,password,avatar});
+
+        //encrypt password
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password,salt); //await means they return a promise
+
+        await user.save(); // if we dont use await then we should use user.save().then....  it is same for salt and user.password as well;
+
+        //return jsonwebtoken
+
+
+        res.send('User registered');
+
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(500).send('server error');
+    }
+
+    
 
 });
 
