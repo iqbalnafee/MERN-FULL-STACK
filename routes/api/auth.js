d2778d69445e3f8757bcfe297493749dcba3e9e9
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-06-08 21:36:21 +0600

    User authentication Login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,6 +4,13 @@ const User = require('../../models/User');
 
 const auth = require('../../middleware/auth');
 
+const jwt = require('jsonwebtoken'); //jwt.io for documentation
+const config = require('config');
+const bcrypt = require('bcryptjs');
+
+
+const { check,validationResult } = require('express-validator');
+
 //@route  GET api/auth
 //@desc   Test route 
 //@access public
@@ -26,6 +33,79 @@ router.get('/',auth, async (req,res) => {
 
 
     //res.send('Auth route');
+
+});
+
+//@route  POST api/auth
+//@desc   Authenticate User & get token
+//@access public 
+
+router.post('/',[
+
+    
+    check('email','Please enter a valid email').isEmail(),
+    check('password','Password is required').exists()
+
+], 
+async (req,res) => {
+
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(400).json({ error: error.array()});
+    }
+
+    //we can get form data by using req.body.name but we dont use it
+
+    //we destructure it by following way because
+
+    const {email,password} = req.body;
+
+    try{
+
+        //see if user exists
+
+        let user = await User.findOne({ email });
+
+        if(!user){
+            return res.status(400).json({ errors: [{msg: 'Invalid credentials'}] });
+        }
+
+        //compare password 
+
+        const isMatch  = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            return res.status(400).json({ errors: [{msg: 'Invalid credentials'}] }); 
+        }
+
+        //return jsonwebtoken
+
+        const payload = {
+
+            user:{
+                id:user.id
+            }
+        };
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),//second param is jwt secret
+            {expiresIn:3600000},
+            (err,token) => {
+                if(err) throw err;
+                res.json({token});
+            }
+            )
+
+
+        //res.send('User registered');
+
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(500).send('server error');
+    }
+
+    
 
 });
 
