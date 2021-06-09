bd99e0c280c8cccbc0fb9078f8631336f2b87dd2
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-06-09 21:01:29 +0600

    get current user's profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-//@route  GET api/profile
-//@desc   Test route 
-//@access public
+const auth = require('../../middleware/auth');
 
-router.get('/', (req,res) => {
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-    res.send('Profile route');
+//@route  GET api/profile/me
+//@desc   To get current user profile 
+//@access private
+
+router.get('/me',auth, async(req,res) => {
+
+    try{
+
+        const profile = await Profile.findOne({user:req.user.id}).populate('user',['name','avatar']);
+        //populate function's 1st parameter is from which model we want to get data and 2nd parameter is an array of objects of table column names
+
+        if(!profile){
+            return res.status(400).json({msg:'There is no profile for this user'});
+        }
+
+        res.json(profile);
+
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 
 });
 
