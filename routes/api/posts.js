8e6715d53ef9396cef12bd676ff811a277e9ccf1
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-06-15 11:17:24 +0600

    like post is done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -149,6 +149,40 @@ router.delete('/:id',auth, async (req, res) => {
 
 });
 
+//@route  PUT api/posts/like/:id
+//@desc  Like a post
+//@access private
+
+router.put('/like/:id',auth, async (req, res) => {
+    try {
+
+        const post = await Post.findById(req.params.id);
+
+        //check if the post has already been liked by the user
+        // if(post.likes.user.toString() === req.user.id){
+        //     return res.status(400).json({msg:'You have already liked this post'});
+        // }
+
+        //this commented out if condition will not work since likes is an arry of user. We will use filter
+
+        if(post.likes.filter(like => like.user.toString()===req.user.id).length>0){
+            return res.status(400).json({msg:'You have already liked this post'});
+        }
+
+        post.likes.unshift({user:req.user.id});
+        await post.save();
+
+        res.json(post.likes);
+        
+    } catch (error) {
+        console.error(error.message);
+        if(error.kind === 'ObjectId'){
+            return res.status(400).json({msg:'Post not found'}); 
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
 
 
 module.exports = router
