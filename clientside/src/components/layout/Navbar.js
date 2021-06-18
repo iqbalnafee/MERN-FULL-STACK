5999aebd5b29fd72e679ced282898b5abf487caa
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-06-18 20:37:35 +0600

    Link router added in Navbar.js

--- clientside/src/components/layout/Navbar.js
+++ clientside/src/components/layout/Navbar.js
@@ -1,17 +1,33 @@
 import React from 'react'
 
+import {BrowserRouter as Router ,Link} from 'react-router-dom'
+
 const Navbar = () => {
     return (
 
+        
+
         <nav className="navbar bg-dark">
-            <h1>
-                <a href="index.html"><i className="fas fa-code"></i> DevConnector</a>
-            </h1>
-            <ul>
-                <li><a href="profiles.html">Developers</a></li>
-                <li><a href="register.html">Register</a></li>
-                <li><a href="login.html">Login</a></li>
-            </ul>
+
+            
+
+            <Router>
+
+                
+
+                <h1>
+                    <Link  to="/"><i className="fas fa-code"></i> DevConnector</Link>
+                </h1>
+                <ul>
+                    <li><Link  to="!#">Developers</Link></li>
+                    <li><Link  to="/register">Register</Link></li>
+                    <li><Link  to="/login">Login</Link></li>
+                </ul>
+                
+
+            </Router>
+
+            
         </nav>
     )
 }
